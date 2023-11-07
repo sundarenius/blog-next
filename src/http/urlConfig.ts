e1a8406d8074460eb9e3c97feab003751c0df6fd
Håkan Sundström <sundarenius@gmail.com>
2023-11-07 19:51:52 +0100

    test

--- src/http/urlConfig.ts
+++ src/http/urlConfig.ts
@@ -8,8 +8,8 @@ const config = {
     apiEndpoint: 'heroku.blabla/api/v1',
   },
   [Environments.DEVELOPMENT]: {
-    // apiEndpoint: 'http://localhost:3030/api/v1',
-    apiEndpoint: 'https://baas-server-production-3c5dcc742018.herokuapp.com/api/v1',
+    apiEndpoint: 'http://localhost:3030/api/v1',
+    // apiEndpoint: 'https://baas-server-production-3c5dcc742018.herokuapp.com/api/v1',
   },
 };
 
