94facf7f86471c47e846e3bc6e0e4a88b5b1157b
Håkan Sundström <sundarenius@gmail.com>
2023-11-09 18:20:24 +0100

    save

--- deploy-jd.sh
+++ deploy-jd.sh
@@ -1,9 +1,13 @@
-echo `NEXT_PUBLIC_LOGO="JWDate.org"
+#!/bin/bash
+
+cat <<EOF > ./.env.production
+NEXT_PUBLIC_LOGO="JWDate.org"
 NEXT_PUBLIC_CUSTOMER_ID="31392486-614f-4a34-bd0f-273305505dc3_TID_-28"
 NEXT_PUBLIC_MAIN_DOMAIN="https://www.jwdate.org"
 NEXT_PUBLIC_TITLE="JWDate.org | Blog and Articles"
 NEXT_PUBLIC_DESCRIPTION="Interesting articles and information about dating topics"
-MY_API_POST_KEY="asdasdmplgnng.fdkfnd97567299--oknkeh"` > ./.env.production &&
+MY_API_POST_KEY="asdasdmplgnng.fdkfnd97567299--oknkeh"
+EOF
 
 echo "yarn build" &&
 yarn build &&
--- deploy-pb.sh
+++ deploy-pb.sh
@@ -1,16 +1,20 @@
-echo `NEXT_PUBLIC_LOGO="PhilippineBeauty.com"
+#!/bin/bash
+
+cat <<EOF > ./.env.production
+NEXT_PUBLIC_LOGO="PhilippineBeauty.com"
 NEXT_PUBLIC_CUSTOMER_ID="276a85c3-06b4-45e4-a409-ee8640cf48ad_TID_-18"
 NEXT_PUBLIC_MAIN_DOMAIN="https://www.philippinebeauty.com"
 NEXT_PUBLIC_TITLE="PhilippineBeauty.com | Blog & Articles"
 NEXT_PUBLIC_DESCRIPTION="Interesting articles and information about dating topics"
-MY_API_POST_KEY="asdasdmplgnng.fdkfnd97567299--oknkeh"` > ./.env.production &&
+MY_API_POST_KEY="asdasdmplgnng.fdkfnd97567299--oknkeh"
+EOF
 
-echo "yarn build" &&
-yarn build &&
-echo "Push to pb-blog-baas" &&
-echo "heroku container:push web -a pb-blog-baas --arg NEXT_PUBLIC_MAIN_DOMAIN=https://www.philippinebeauty.com, NEXT_PUBLIC_CUSTOMER_ID=276a85c3-06b4-45e4-a409-ee8640cf48ad_TID_-18" &&
-heroku container:push web -a pb-blog-baas --arg NEXT_PUBLIC_MAIN_DOMAIN="https://www.philippinebeauty.com",NEXT_PUBLIC_CUSTOMER_ID="276a85c3-06b4-45e4-a409-ee8640cf48ad_TID_-18" &&
-echo "container:release web -a pb-blog-baas:" &&
-heroku container:release web -a pb-blog-baas
+# echo "yarn build" &&
+# yarn build &&
+# echo "Push to pb-blog-baas" &&
+# echo "heroku container:push web -a pb-blog-baas --arg NEXT_PUBLIC_MAIN_DOMAIN=https://www.philippinebeauty.com, NEXT_PUBLIC_CUSTOMER_ID=276a85c3-06b4-45e4-a409-ee8640cf48ad_TID_-18" &&
+# heroku container:push web -a pb-blog-baas --arg NEXT_PUBLIC_MAIN_DOMAIN="https://www.philippinebeauty.com",NEXT_PUBLIC_CUSTOMER_ID="276a85c3-06b4-45e4-a409-ee8640cf48ad_TID_-18" &&
+# echo "container:release web -a pb-blog-baas:" &&
+# heroku container:release web -a pb-blog-baas
 
-echo "Deployed!"
+# echo "Deployed!"
