fc2cfb7a4a2c245b057b8b60d4698bd4e5ad7143
Håkan Sundström <sundarenius@gmail.com>
2023-11-09 16:25:00 +0100

    fix

---
+++ deploy-pb.sh
@@ -0,0 +1,11 @@
+export NEXT_PUBLIC_CUSTOMER_ID="276a85c3-06b4-45e4-a409-ee8640cf48ad_TID_-18" &&
+export NEXT_PUBLIC_MAIN_DOMAIN="https://www.philippinebeauty.com" &&
+echo "yarn build" &&
+yarn build &&
+echo "Push to pb-blog-baas" &&
+echo "heroku container:push web -a pb-blog-baas --arg NEXT_PUBLIC_MAIN_DOMAIN=https://www.philippinebeauty.com, NEXT_PUBLIC_CUSTOMER_ID=276a85c3-06b4-45e4-a409-ee8640cf48ad_TID_-18" &&
+heroku container:push web -a pb-blog-baas --arg NEXT_PUBLIC_MAIN_DOMAIN="https://www.philippinebeauty.com",NEXT_PUBLIC_CUSTOMER_ID="276a85c3-06b4-45e4-a409-ee8640cf48ad_TID_-18" &&
+echo "container:release web -a pb-blog-baas:" &&
+heroku container:release web -a pb-blog-baas
+
+echo "Deployed!"
