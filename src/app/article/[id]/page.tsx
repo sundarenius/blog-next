0a95563dba8007df167c6bce785b28d96251ad50
Håkan Sundström <sundarenius@gmail.com>
2023-11-15 12:51:52 +0100

    height

--- src/app/article/[id]/page.tsx
+++ src/app/article/[id]/page.tsx
@@ -39,7 +39,7 @@ const ArticlePage = async ({ params }: any) => {
           <img
             src={pictureUrl}
             alt={tag || 'article'}
-            style={{ height: '500px', maxWidth: '90vw' }}
+            style={{ maxHeight: '500px', maxWidth: '90vw' }}
           />
 
           <TextContent content={content} />
